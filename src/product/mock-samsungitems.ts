5074a30353c3661307c7186686bbaf25e37b9389
Nilesh-Burla1106 <141109890+Nilesh-Burla1106@users.noreply.github.com>
2023-08-15 13:12:02 +0530

    1 file modified

--- src/product/mock-samsungitems.ts
+++ src/product/mock-samsungitems.ts
@@ -1,5 +1,7 @@
 import { Samsung } from "./samsung";
 
 export const SAMSUNGITEMS:Samsung[]=[
-  
+  {id:1,name:"laptop",field:"electronics",avatar:"https://5.imimg.com/data5/MA/LI/MY-58087416/samsung-chromebook.jpeg",info:"Samsung laptops are equipped with advanced features and technologies, such as vibrant displays with high resolution, immersive audio systems, and fast storage options"},
+  {id:2,name:"mobiles",field:"electronics",avatar:"https://images.unsplash.com/photo-1610945265064-0e34e5519bbf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c2Ftc3VuZyUyMHBob25lfGVufDB8fDB8fHww&w=1000&q=80",info:"Every genuine mobile phone has a serial number to register it to a carrier network. This number is called the International Mobile Equipment Identity (IMEI) number"},
+  {id:3,name:"tablets",field:"electronics",avatar:"https://image-us.samsung.com/SamsungUS/home/mobile/tablets/5102022/GalaxyTabA8_DarkGray_Combo.png?$product-details-jpg$",info:"You can read and send emails, access the Internet, take and edit photos, read E-books, and access different apps installed"},
 ];
