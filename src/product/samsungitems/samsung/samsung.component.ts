5185105dd999086828b7f7f1b153c07c749bed9f
Nilesh-Burla1106 <141109890+Nilesh-Burla1106@users.noreply.github.com>
2023-08-15 14:53:01 +0530

    2 files modified, 3 added

---
+++ src/product/samsungitems/samsung/samsung.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+// import { Samsung } from 'src/product/samsung';
+
+@Component({
+  standalone:true,
+  selector: 'app-samsung',
+  templateUrl: './samsung.component.html',
+  styleUrls: ['./samsung.component.css']
+})
+export class SamsungComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+
+}
--- src/product/samsungitems/samsungitems.component.ts
+++ src/product/samsungitems/samsungitems.component.ts
@@ -1,16 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { SAMSUNGITEMS } from '../mock-samsungitems';
+import { Samsung } from '../samsung';
+import { SamsungComponent } from './samsung/samsung.component';
 
 @Component({
   standalone:true,
   selector: 'app-samsungitems',
   templateUrl: './samsungitems.component.html',
-  styleUrls: ['./samsungitems.component.css']
+  styleUrls: ['./samsungitems.component.css'],
+  imports:[SamsungComponent,CommonModule],
 })
 export class SamsungitemsComponent implements OnInit {
-
+  samsungitems:Samsung[]=[];
   constructor() { }
 
   ngOnInit() {
+    this.samsungitems=SAMSUNGITEMS;
   }
 
 }
